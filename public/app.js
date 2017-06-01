e6ee076dcf7af4b54da789b996807df5e5ec5c5b
Soniya Jassal <jassal_soniya@bah.com>
2017-06-01 09:48:13 -0400

    Adding Angular

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 
-// console.clear();
+console.clear();
+
 //================================
 //-----API Domains Dev & Prod-----
 //================================
-
 var frontend_domain = "http://localhost:8000/";
 var backend_domain = "http://localhost:3000/";
 // var frontend_domain = 'https://shielded-journey-frontend-40635.herokuapp.com/';
@@ -27,20 +27,167 @@ app.controller('StorytellerController',['$http',function($http){
 //========================
 app.controller('StoryController', ['$http', function($http){
 
+   this.storyCategory = '';
+   this.date_created = '';
+   this.title = '';
+   this.img = '';
+   this.description = '';
+   this.storyContent = '';
+   this.categories = [];
+   this.snippets = [];
+   this.recentSnippets = [];
+   this.stories = [];
+   this.snippetTitle = [];
+   this.allSnippetsTitle =[];
    this.category = [];
+   this.createStory = false;
    this.recentSnippet = false;
+   this.allStories = false;
+   this.addSnippet = false;
 
+   //==============================
+   //----Story: Get All Stories----
+   //==============================
 
-   this.name = 'Soniya Jassal';
-   this.date_created = 'May 26, 2017';
-   this.title = 'Once Upon a Time...';
-   this.img = 'https://s-media-cache-ak0.pinimg.com/736x/e9/08/76/e908769e80d88f74139c4ebb84738229.jpg'
-   this.description = 'This is a description of the story to be built'
-   this.storyContent = 'Once Upon a Time tells the story of a new world, one in which fairy-tale legends and modern life collide.';
-   this.category = ['Fantasy', 'Sci-Fi', 'Historical', 'Biographical']
-   this.snippet = ['There was a fairy lawyer who was all feeling a woe', 'She traveld far and wide to find her Troll', 'But alas all she could find was a man who bowled.']
+   $http({
+     method: 'GET',
+     url: backend_domain + '/stories'
+   }).then(function(result){
+   //   var today = new Date();
+   //   var date = today.getFullYear() + '-' + (today.getMonth() + 1 ) + '-' + today.getDate() + 'T';
+   //   var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+     //console.log('Stories: ', result);
+     this.stories = result.data.reverse();
+     console.log(this.addSnippet);
+   //   localStorage.setItem('', this.stories);
+     this.categories = result.data;
+     this.setRecentSnippetTitle();
+   }.bind(this));
 
-   this.recentSnippets = function(){
-      this.recentSnippet = true;
+   //===========================
+   //---Story: Create Stories---
+   //===========================
+   this.newStory = function(){
+      $http({ // Makes HTTP request to server
+        method: 'POST',
+        url: backend_domain + '/stories',
+        data: {
+          story: { // Gets turned into req.body
+           title: this.title,
+           description: this.description,
+           content: this.content,
+           category: this.category,
+           img: this.image
+          }
+        }
+      }).then(function(response) {
+        //console.log(response);
+      }.bind(this));
    }
+
+   //===============================
+   //----Story: Get All Snippets----
+   //===============================
+
+   $http({
+     method: 'GET',
+     url: backend_domain + '/snippets'
+   }).then(function(result){
+     //console.log('Snippets: ',result.data);
+   //   console.log(JSON.parse(localStorage.getItem('stories')));
+     this.snippets = result.data;
+      //this.recentSnippets = result.data; //if created by is within a certain period time - TBD
+   }.bind(this));
+
+   //==============================================
+   //---Story: Function Set Recent Snippet Title---
+   //==============================================
+   this.setRecentSnippetTitle = function(){
+      //console.log('This.Snippets: ',this.snippets);
+    //   this.setSnippetTitle();
+      //console.log('This.Stories: ', this.stories);
+      for (var i = this.snippets.length - 1; i > (this.snippets.length - 6) ; i--) {
+             this.recentSnippets.push(this.snippets[i]);
+             //console.log('Recent Snippets: ',this.recentSnippets);
+      }
+      for (var i = 0; i < this.recentSnippets.length; i++) {
+          //console.log('In FOR loop', i);
+          for (var j = 0; j < this.stories.length; j++) {
+             //console.log('In second FOR loop', j);
+             //console.log('Recent Snippet Story ID ', this.recentSnippets[i].story_id );
+             //console.log('Stories Story ID ', this.stories[j].id);
+             if (this.recentSnippets[i].story_id === this.stories[j].id){
+                 //console.log('In IF statement [i], [j], storytitle', i,j,this.stories[i].title);
+                 this.snippetTitle.push(this.stories[j].title);
+                 //console.log('Snippet Title after push: ',this.snippetTitle);
+             }
+
+          }
+
+       }
+   }
+   //=======================================
+   //---Story: Function Show All Snippets---
+   //=======================================
+   this.showSnippets = function(){
+      // console.log('recentSnippet: '+ this.recentSnippet);
+      // console.log('allStories: '+ this.allStories);
+      this.recentSnippet = true;  //show
+      // console.log('recentSnippet: '+ this.recentSnippet);
+      this.allStories = true; //hide
+      this.createStory = false;
+      // console.log('allStories: '+ this.allStories);
+      this. displaySnippets();
+   }
+   //==========================================
+   //---Story: Function Display All Snippets---
+   //==========================================
+   this.displaySnippets = function(){
+      console.log('Snips: ',this.snippets);
+      console.log('Stories: ', this.stories);
+
+      for (var i = 0; i < this.snippets.length; i++) {
+          //console.log('In FOR loop', i);
+          for (var j = 0; j < this.stories.length; j++) {
+             //console.log('In second FOR loop', j);
+             //console.log('Recent Snippet Story ID ', this.recentSnippets[i].story_id );
+             //console.log('Stories Story ID ', this.stories[j].id);
+             if (this.snippets[i].story_id === this.stories[j].id){
+                 //console.log('In IF statement [i], [j], storytitle', i,j,this.stories[i].title);
+                 this.allSnippetsTitle.push(this.stories[j].title);
+                 //console.log('Snippet Title after push: ',this.snippetTitle);
+             }
+          }
+       }
+   }
+
+   this.addSnippets = function(){
+      this.addSnippet = true;
+      console.log(this.addSnippet);
+      this.allStories = true;
+   }
+   this.goHome = function(){
+      this.recentSnippet = false;  //show
+      this.allStories = false; //hide
+      this.createStory = false;
+   }
+
+   this.createStories = function(){
+      this.createStory = true;
+      this.allStories = true;
+      this.recentSnippet = false; //show form
+   }
+
+   this.setIndex = function(index){
+      console.log('tile: ', this.story[index].title);
+      console.log('tile: ', this.story[index].description);
+      console.log('tile: ', this.story[index].category);
+      console.log('tile: ', this.story[index].img);
+      this.title = this.story[index].title;
+      this.description = this.story[index].description;
+      this.storyCategory = this.story[index].category;
+      this.img = this.story[index].img;
+
+   }
+
 }]);
